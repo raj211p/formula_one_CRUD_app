b3ababb81a1d5481b85d962f8cdf9ea6d903de0c
Prathyush R <97680273+raj211p@users.noreply.github.com>
2022-03-12 00:35:34 +0530

    Add files via upload

---
+++ formula1/partial/car_nav.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-default navbar-fixed-top topnav navbar-light bg-light">
+ <a class="navbar-brand" href="/formula1">Home</a>
+ 
+
+ <div id="navbarNav" style="text-align:left;" class="container-fluid topnav"> 
+   <a class="nav-link" href="/formula1/car_add.php">Add a new car</a>
+   <a class="nav-link" href="/formula1/race.php">Races</a>
+   <a class="nav-link" href="/formula1/car.php">Cars</a>
+   <a class="nav-link" href="/formula1/pit_crew.php">Pit crew</a>
+   <a class="nav-link" href="/formula1/driver.php">Drivers</a>
+   <a class="nav-link" href="/formula1/cars_pit_crew.php">Pit crew/Cars</a>
+   <a class="nav-link" href="/formula1/teams_races.php">Teams/races</a>
+ </div>
+</nav>
+
---
+++ formula1/partial/head.php
@@ -0,0 +1,11 @@
+<head>
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible">
+ <title>Formula One team management system</title>
+ <link rel="stylesheet" href="partial/bootstrap.min.css">
+ <style>
+   .formdiv {margin:0 auto; width: 40%}
+   .info {height: 20px;}
+ </style>
+</head>
